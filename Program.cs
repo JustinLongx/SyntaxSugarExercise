5396c127709992ca979b774c47597b7dce5090b1
justin long <justinxlong93@gmail.com>
2024-11-20 10:08:27 -0600

    finished exercise

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.NetworkInformation;
+
+namespace SyntaxSugarExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Given the following code, change the code to implement:
+
+            //+ inferred typing
+            //+ string interpolation
+            //+ and the ternary operator
+
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than nine";
+            Console.WriteLine(response);
+
+        }
+    }
+}
